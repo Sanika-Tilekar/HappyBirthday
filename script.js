990ef90bf326c6ab628a4a44e07082a88411520f
Sanika-Tilekar <sanikatilekar2004@gmail.com>
2025-04-12 19:46:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("revealMessageBtn").addEventListener("click", function() {
+    const specialMessage = document.getElementById("specialMessage");
+    specialMessage.classList.toggle("hidden");
+});
